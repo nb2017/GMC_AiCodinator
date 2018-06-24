bb58c171721a3cfffb17ec066da5bd4694511ff2
nb2017 <zerondrd@gmail.com>
2018-06-24 12:36:04 +0900

    要らないやつ削除

--- AI_Codinator/target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO custom(first_name, last_name, pass_word) VALUES('Nobita', 'Nobi', '1111');
-INSERT INTO custom(first_name, last_name, pass_word) VALUES('Takeshi', 'Goda', '1111');
-INSERT INTO custom(first_name, last_name, pass_word) VALUES('Suneo', 'Honekawa', '1111');
-INSERT INTO custom(first_name, last_name, pass_word) VALUES('Shizuka', 'Minamoto', '1111');
-INSERT INTO itemtable(item_name, img_path) VALUES('スニーカー白', '/images/1.jpg');
-INSERT INTO itemtable(item_name, img_path) VALUES('スニーカー緑', '/images/1.jpg');
