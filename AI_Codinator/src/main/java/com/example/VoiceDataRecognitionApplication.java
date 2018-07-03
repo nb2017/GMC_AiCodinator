83f9f726bc77a900e13ffbbd07241261871266a1
sakanobe <zerondrd@gmail.com>
2018-07-03 21:05:39 +0900

    音声入力文字列情報収集システム

--- AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
+++ AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiCodinatorApplication {
+public class VoiceDataRecognitionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiCodinatorApplication.class, args);
+		SpringApplication.run(VoiceDataRecognitionApplication.class, args);
 	}
 }
