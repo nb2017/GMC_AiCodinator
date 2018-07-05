04356c2e204b9563abe3f392bed40d09e4d7e702
JohnTItor <zerondrd@gmail.com>
2018-07-05 21:42:19 +0900

    test

--- AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
+++ AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VoiceDataRecognitionApplication {
 
+	@GetMapping("/")
+	string home()
+	{
+
+
+		return "Hello";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(VoiceDataRecognitionApplication.class, args);
 	}
