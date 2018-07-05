88eb37d30fb137593e2df38b4ab2ce0966b3ff86
sakanobe <zerondrd@gmail.com>
2018-07-05 23:51:07 +0900

    ケイスケホンダ的なやつ

--- AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
+++ AI_Codinator/src/main/java/com/example/VoiceDataRecognitionApplication.java
@@ -3,10 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VoiceDataRecognitionApplication {
-
+	@GetMapping("/")
+	String Home()
+	{
+		// ここにテスト的にはDBの内容を返してあげよう
+		return "Hello!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VoiceDataRecognitionApplication.class, args);
 	}
